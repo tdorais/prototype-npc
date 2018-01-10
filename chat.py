e0e84b87f3e81b4a342921505d4d624f582f013c
unknown <tdorais@gmail.com>
2018-01-09 21:57:06 -0800

    Basic loading and first message

--- chat.py
+++ chat.py
@@ -1,4 +1,20 @@
 """Begins the conversation with Ship."""
+from pathlib import Path
 import json
 
-print("Good Morning, Captain")
+def load_messages():
+    """Loads messages"""
+    messages = json.load(open("lib/messages.json"))
+    return messages
+
+def load_player():
+    """Loads player data."""
+    player_file = Path("sav/player.json")
+
+    if not player_file.is_file():
+        name = input("Hello, I am Ship. How may I refer to you?\n")
+
+    print("Good Morning, " + name)
+    return
+
+load_player()
