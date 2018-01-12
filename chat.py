49a05904062b9c6c51fd17b0a33c3ebab5ea4a55
Tim <tdorais@gmail.com>
2018-01-11 23:06:21 -0800

    Added init and ignored pycache

--- chat.py
+++ chat.py
@@ -3,14 +3,20 @@ import json
 import os
 from bin import player
 
+def cls():os.system('cls' if os.name=='nt' else 'clear')
+
 def load_messages():
     """Loads messages"""
     messages = json.load(open("lib/messages.json"))
     return messages
 
-def cls():os.system('cls' if os.name=='nt' else 'clear')
+def init():
+    'Prep the game up.'
+    cls()
+    os.makedirs("sav", exist_ok=True)
+    load_messages()
 
-cls()
+init()
 you = player.Player()
 cls()
 print(you.get_greeting())
